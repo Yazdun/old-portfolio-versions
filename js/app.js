856201f72ce01bae44756cae7b0c26453ae01552
Yazdun <yazduntube@gmail.com>
2022-02-19 17:38:16 +0330

    commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function () {
+  document.getElementById("main").classList.remove("display__none");
+  document.getElementById("loading").classList = "display__none";
+});
